2bc61904edcc16044f2e98e1c6119be536c27698
Kostiuk97 <144224782+Kostiuk97@users.noreply.github.com>
2023-11-30 15:48:43 +0200

    Add files via upload

--- practikal6.py
+++ practikal6.py
@@ -1,11 +1,17 @@
 import math
 
-def data(file_path='input.txt'):
-    with open(file_path, 'r') as file:
-        x = float(file.read())
-        import math
-x = 0.9
-n = 1.6
-w = abs(1 - math.sin(2*x) + n)
-t = 2*w + 1/7**3
-print(f'n = {n}, x = {x}, w = {w}, t = {t}')
+def data(file_path='0.txt'):
+    with open(file_path ,'r') as file:
+        numbers = file.readline().split() 
+        x = float(numbers[0])
+        n = float(numbers[1])
+        w = abs(1 - math.sin(2*x) + n)  
+        t = 2*w + 1/7**3
+    return x, n, w, t
+
+result = data()
+
+result_str = f"x: {result[0]}\n n: {result[1]}\n w: {result[2]}\n t: {result[3]}"
+print(result_str)
+with open("output.txt", 'w') as f:
+    f.write(result_str)
